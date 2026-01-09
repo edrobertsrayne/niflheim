be369c632be2652d986045a17477b7b622946c17
Ed Roberts Rayne <ed.rayne@gmail.com>
2026-01-09 22:27:49 +0000

    feat(bat): enable tokyonight theme

--- modules/utilities/bat.nix
+++ modules/utilities/bat.nix
@@ -1,6 +1,22 @@
 _: {
-  flake.modules.homeManager.utilities = {
-    programs.bat.enable = true;
+  flake.modules.homeManager.utilities = {pkgs, ...}: {
+    programs.bat = {
+      enable = true;
+      themes = {
+        tokyonight = {
+          src = pkgs.fetchFromGitHub {
+            owner = "folke";
+            repo = "tokyonight.nvim";
+            rev = "5da1b76e64daf4c5d410f06bcb6b9cb640da7dfd";
+            sha256 = "4zfkv3egdWJ/GCWUehV0MAIXxsrGT82Wd1Qqj1SCGOk=";
+          };
+          file = "extras/sublime/tokyonight_night.tmTheme";
+        };
+      };
+      config = {
+        theme = "tokyonight";
+      };
+    };
     home.shellAliases = {
       cat = "bat";
     };
