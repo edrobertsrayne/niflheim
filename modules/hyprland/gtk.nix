1986a6477639e9b61403d8b4ff9cef022cc63792
Ed Roberts Rayne <ed.rayne@gmail.com>
2026-01-10 21:40:56 +0000

    fix(gtk): fix incorrect settings for gtk3 and set adw-gtk3-dark theme

--- modules/hyprland/gtk.nix
+++ modules/hyprland/gtk.nix
@@ -4,26 +4,29 @@ in {
   flake.modules.homeManager.hyprland = {pkgs, ...}: let
     settings = ''
       [Settings]
-      gtk-theme-name=adw-gtk3
-      gtk-application-prefer-dark-theme = true
+      gtk-theme-name=adw-gtk3-dark
       gtk-icon-theme-name=Papirus-Dark
       gtk-cursor-theme-name=Bibata-Modern-Classic
       gtk-cursor-theme-size=24
       gtk-font-name=${fonts.sans.name} 11
-      gtk-font-antialiasing=rgba
-      gtk-font-rgba-order=rgb
-      gtk-font-scaling-factor=1.0
     '';
   in {
     home.packages = with pkgs; [
       adw-gtk3
       bibata-cursors
-      papirus-folders
       papirus-icon-theme
     ];
     xdg.configFile = {
-      "gtk-3.0/settings.ini".text = settings;
+      "gtk-3.0/settings.ini".text =
+        settings
+        + ''
+          gtk-xft-antialias=1
+          gtk-xft-hinting=1
+          gtk-xft-hintstyle=hintslight
+          gtk-xft-rgba=rgb
+        '';
       "gtk-4.0/settings.ini".text = settings;
+      # TODO: GTK4 font antialisaing using fontconfig
     };
   };
 }
